32e181301ed7d934fb8e959c1e1f3124e22cee1b
Yijun Huang <yijun.huang@mail.utoronto.ca>
2024-09-25 19:26:28 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -2,15 +2,23 @@ public class Driver {
     public static void main(String [] args) {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
+        double [] c1 = {6,2,3,5};
+        int[] e1 = {0, 2, 3, 6};
+        Polynomial p1 = new Polynomial(c1, e1);
+        double [] c2 = {-2, -9, 3};
+        int[] e2 = {1, 3, 4};
+        Polynomial p2 = new Polynomial(c2, e2);
         Polynomial s = p1.add(p2);
+        Polynomial m = p1.multiply(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
+        System.out.println("m(0.1) = " + m.evaluate(0.1));
         if(s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+        if(m.hasRoot(0))
+            System.out.println("0 is a root of m");
+        else
+            System.out.println("0 is not a root of m");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,44 +1,130 @@
 public class Polynomial {
     double[] coefficients;
+    int[] exponents;
 
     public Polynomial() {
         coefficients = new double[1];
         coefficients[0] = 0.0;
+        exponents = new int[1];
+        exponents[0] = 0;
     }
 
-    public Polynomial(double[] coefficients) {
+    public Polynomial(double[] coefficients, int[] exponents) {
         this.coefficients = new double[coefficients.length];
+        this.exponents = new int[exponents.length];
         for(int i = 0; i < coefficients.length; i++){
             this.coefficients[i] = coefficients[i];
         }
+        for(int i = 0; i < exponents.length; i++) {
+            this.exponents[i] = exponents[i];
+        }
+    }
+
+    public double add_find(double[] coe, int[] exp, int target) {
+        for(int i = 0; i < coe.length; i++) {
+            if(exp[i] == target) {
+                return coe[i];
+            }
+        }
+        return 0;
     }
 
     public Polynomial add(Polynomial p) {
-        double[] result;
-        int max_length;
-        if(this.coefficients.length >= p.coefficients.length) {
-            max_length = this.coefficients.length;
+        int max_length = -1;
+        for(int i = 0; i < this.exponents.length; i++) {
+            if(this.exponents[i] > max_length) {
+                max_length = this.exponents[i];
+            }
         }
-        else {
-            max_length = p.coefficients.length;
+        for(int i = 0; i < p.exponents.length; i++) {
+            if(p.exponents[i] > max_length) {
+                max_length = p.exponents[i];
+            }
         }
-        result = new double[max_length];
+        max_length++;
+        boolean[] temp_exp = new boolean[max_length];
+        for(int i = 0; i < this.exponents.length; i++) {
+            temp_exp[this.exponents[i]] = true;
+        }
+        for(int i = 0; i < p.exponents.length; i++) {
+            temp_exp[p.exponents[i]] = true;
+        }
+        int count = 0;
         for(int i = 0; i < max_length; i++) {
-            if (i < this.coefficients.length) {
-                result[i] += this.coefficients[i];
-            }
-            if (i < p.coefficients.length) {
-                result[i] += p.coefficients[i];
+            if(temp_exp[i]) {
+                count++;
             }
         }
-        Polynomial r = new Polynomial(result);
-        return r;
+        int[] result_exp = new int[count];
+        double[] result_coe = new double[count];
+        int pointer = 0;
+        for(int i = 0; i < max_length; i++) {
+            if(temp_exp[i]) {
+                result_exp[pointer] = i;
+                pointer++;
+            }
+        }
+        for(int i = 0; i < count; i++) {
+            result_coe[i] = add_find(this.coefficients, this.exponents, result_exp[i]) + add_find(p.coefficients, p.exponents, result_exp[i]);
+        }
+        Polynomial result = new Polynomial(result_coe, result_exp);
+        return result;
+    }
+
+    public double mult_find(double[] c1, int[] e1, double[] c2, int[] e2, int target) {
+        double result = 0;
+        for(int i = 0; i < c1.length; i++) {
+            for(int j = 0; j < c2.length; j++) {
+                if(e1[i] + e2[j] == target) {
+                    result += c1[i] * c2[j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public Polynomial multiply(Polynomial p) {
+        int max_exp = -1;
+        for(int i = 0; i < this.exponents.length; i++) {
+            for(int j = 0; j < p.exponents.length; j++) {
+                if(this.exponents[i] + p.exponents[j] > max_exp) {
+                    max_exp = this.exponents[i] + p.exponents[j];
+                }
+            }
+        }
+        max_exp++;
+        boolean[] temp_exp = new boolean[max_exp];
+        for(int i = 0; i < this.exponents.length; i++) {
+            for(int j = 0; j < p.exponents.length; j++) {
+                temp_exp[this.exponents[i] + p.exponents[j]] = true;
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < max_exp; i++) {
+            if(temp_exp[i]) {
+                count++;
+            }
+        }
+        int[] result_exp = new int[count];
+        double[] result_coe = new double[count];
+        int pointer = 0;
+        for(int i = 0; i < max_exp; i++) {
+            if(temp_exp[i]) {
+                result_exp[pointer] = i;
+                pointer++;
+            }
+        }
+        for(int i = 0; i < count; i++) {
+            result_coe[i] = mult_find(this.coefficients, this.exponents, p.coefficients, p.exponents, result_exp[i]);
+        }
+        Polynomial result = new Polynomial(result_coe, result_exp);
+        return result;
     }
 
     public double evaluate(double x) {
         double result = 0.0;
         for(int i = 0; i < this.coefficients.length; i++) {
-            result += Math.pow(x, i) * this.coefficients[i];
+            result += Math.pow(x, this.exponents[i]) * this.coefficients[i];
         }
         return result;
     }
